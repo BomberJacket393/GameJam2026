0a68817899f03a12fbabe6b6773e640f517f7995
Tyler <tyler.lynch06@icloud.com>
2026-07-04 00:37:02 +0100

    Changed wave system

--- round_handler.gd
+++ round_handler.gd
@@ -2,8 +2,10 @@ extends Node
 
 class_name round_handler
 
+##Whole round system could do with rework, batch system was a mistep
+
 @export var enemiesPerRound : Array[int]
-@export var currentEnemiesInRound : int
+@export var enemiesLeftToSpawn : int
 @export var currentRound : int
 @export var enemyTarget : Node2D
 @export var spawners : Array[Node]
@@ -13,7 +15,12 @@ class_name round_handler
 @export var batchReleaseDelayFactor : float
 var inIntermission : bool = false
 var intermissionTimer : float
-var timeBetweenBatches
+var timeBetweenBatches : float
+##When there are no enemies, Wait a few seconds
+##If still no enemies, persist to next round 
+@export var timeBeforeEndingRound : float
+var timeWithNoLivingEnemies : float
+var enemiesConfirmedDead : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,19 +29,26 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if currentEnemiesInRound <= 0 and not inIntermission:
+	var livingEnemyCount = get_tree().get_nodes_in_group("enemies").size()
+	if livingEnemyCount == 0 and not inIntermission:
+		timeWithNoLivingEnemies += delta
+		if timeWithNoLivingEnemies > timeBeforeEndingRound:
+			enemiesConfirmedDead = true
+	else:
+		timeWithNoLivingEnemies = 0 
+		
+	if enemiesLeftToSpawn <= 0 and enemiesConfirmedDead and not inIntermission:
 		roundOver()
-		inIntermission = true
 	if inIntermission:
 		intermissionTimer -= delta
 		if intermissionTimer <= 0:
 			startRound() 
 	
 func getEnemiesInRound():
-	return currentEnemiesInRound
+	return enemiesLeftToSpawn
 
 func decrementEnemy():
-	currentEnemiesInRound -= 1
+	enemiesLeftToSpawn -= 1
 
 func roundOver():
 	collectSpawners()
@@ -43,15 +57,17 @@ func roundOver():
 		spawner.roundOver()
 	currentRound += 1
 	inIntermission = true
+	enemiesConfirmedDead = false
 	intermissionTimer = intermissionTime	
 	
 func startRound():
+	print("BEGINNING_ROUND_"+str(currentRound))
 	collectSpawners()
 	inIntermission = false
-	currentEnemiesInRound = enemiesPerRound[currentRound]
-	timeBetweenBatches = timeBetweenBatchesFactor * (1 / currentEnemiesInRound)
+	enemiesLeftToSpawn = enemiesPerRound[currentRound]
+
 	for spawner in spawners:
-		spawner.roundStart(timeBetweenBatches, currentEnemiesInRound * enemySpeedFactor, batchReleaseDelayFactor * 1 / currentEnemiesInRound)
+		spawner.roundStart(enemiesLeftToSpawn * enemySpeedFactor, 1/(1+currentRound))
 	
 func collectSpawners():
 	spawners = get_tree().get_nodes_in_group("spawners")
--- scenes/enemy_spawner.gd
+++ scenes/enemy_spawner.gd
@@ -4,62 +4,28 @@ extends Node2D
 @export var enemy : PackedScene
 ##Shared round handler accross all spawners
 @export var roundHandler : round_handler
-@export var enemiesInBatchRange : Array[int]
-@export var currentBatchSize : int
-var currentBatchReleaseDelay : float = 0.3
-var batchReleaseTimer : float
-var releasingBatch : bool
 var enemySpeed : float
 @export var isIntermission : bool
-var timeBetweenBatches : float
-@export var batchSizeRoundIncrement : int
-
 var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	timer.timeout.connect(processBatch)
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	timer.paused = releasingBatch or isIntermission
-	if not isIntermission:
-		if releasingBatch and batchReleaseTimer <= 0:
-			spawnEnemy()
-			currentBatchSize -= 1
-			roundHandler.decrementEnemy()
-			batchReleaseTimer = currentBatchReleaseDelay
-		if releasingBatch:
-			batchReleaseTimer -= delta
-			
-		if currentBatchSize <= 0 and releasingBatch:
-			releasingBatch = false
-			currentBatchSize = 0
-	
-func processBatch():
-	releasingBatch = true
-	currentBatchSize = rng.randi_range(enemiesInBatchRange[0],enemiesInBatchRange[1])
-	#print(str(name).to_upper()+" BEGINNING BATCH RELEASE OF SIZE "+str(currentBatchSize))
-	batchReleaseTimer = 0
+	timer.timeout.connect(spawnEnemy)
 	
 func spawnEnemy():
-	var enemyInstance = enemy.instantiate()
-	enemyInstance.position = position
-	enemyInstance.target = roundHandler.enemyTarget
-	enemyInstance.speed = enemySpeed
-	get_tree().current_scene.add_child(enemyInstance)
-	releasingBatch = true
+	if not isIntermission and roundHandler.getEnemiesInRound()>0:
+		roundHandler.decrementEnemy()
+		var enemyInstance = enemy.instantiate()
+		enemyInstance.position = position
+		enemyInstance.target = roundHandler.enemyTarget
+		enemyInstance.speed = enemySpeed
+		get_tree().current_scene.add_child(enemyInstance)
 	
-func roundStart(_timeBetweenBatches, _enemySpeed, _batchReleaseDelay):
+func roundStart(_enemySpeed, spawnDelay):
 	enemySpeed = _enemySpeed
-	timeBetweenBatches = _timeBetweenBatches
-	timer.wait_time = timeBetweenBatches
-	currentBatchReleaseDelay = _batchReleaseDelay
+	timer.wait_time = spawnDelay
 	isIntermission = false
 	
 func roundOver():
 	isIntermission = true
-	currentBatchSize = 0
-	enemiesInBatchRange[0] += batchSizeRoundIncrement
-	enemiesInBatchRange[1] += batchSizeRoundIncrement
 		
