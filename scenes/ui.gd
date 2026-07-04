fc432748a9cb7caad2814a5de8c88ae55024f847
MttLtt18 <matthewlittlewood3@gmail.com>
2026-07-04 22:25:35 +0100

    fix to scaling resolutions

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -5,11 +5,6 @@ func _ready():
 	$CanvasLayer/TutorialHint2.hide()
 	$CanvasLayer/TutorialHint3.hide()
 	$AudioStreamPlayer2D.play()
-	
-func _process(delta):
-	$CanvasLayer/Attention/TextureRect/ProgressBar.value = PD.availablePower
-	$CanvasLayer/Water/TextureRect/ProgressBar.max_value = PD.maxPower
-
 func _on_texture_button_button_down():
 	$CanvasLayer/TutorialHint.hide()
 	$CanvasLayer/TutorialHint2.show()
