e4b69e0c76c7e07f2e34278081afba5a3245cd7a
Tyler <tyler.lynch06@icloud.com>
2026-07-04 23:47:15 +0100

    minor changes

--- economy.gd
+++ economy.gd
@@ -1,9 +1,12 @@
 extends Node
 
 var water = 500 
+var cursor
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if PD.cursor != null:
+		cursor = PD.cursor
 	pass # Replace with function body.
 
 
